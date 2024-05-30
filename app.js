5c25094031ac62932826abcc82430c03f549c7f0
Karan Pal <pal.karan@aicerts.io>
2024-05-30 23:27:01 +0530

    Api fix

--- app.js
+++ app.js
@@ -33,9 +33,9 @@ app.options('*', cors());
 app.use('/v1', routes);
 
 // send back a 404 error for any unknown api request
-app.use((req, res, next) => {
-    next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
-});
+// app.use((req, res, next) => {
+//     next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
+// });
 
 // convert error to ApiError, if needed
 app.use(errorConverter);
