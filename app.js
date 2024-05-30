d0679aca5f703d9efd034cb1d6e938508af82e5f
Karan Pal <pal.karan@aicerts.io>
2024-05-30 23:32:19 +0530

    migrated app.js

--- app.js
+++ app.js
@@ -1,46 +1,100 @@
-const express = require('express')
-const helmet = require('helmet')
-const xss = require('xss-clean')
-const cors = require('cors')
-const httpStatus = require('http-status')
-const config = require('./config/config')
-const morgan = require('./config/morgan')
-const compression = require('compression')
-const { errorConverter, errorHandler } = require('./middlewares/error')
-const ApiError = require('./utils/ApiError')
-const mongoSanitize = require('express-mongo-sanitize')
-const routes = require('./routes')
+const mongoose = require('mongoose')
+const app = require('./index');
+const config = require('./config/config');
+const logger = require('./config/logger');
+const axios = require('axios')
+const Crypto = require('./models/cryptoModel')
+const cron = require('node-cron')
 
-const app = express();
+const validateCryptoData = (cryptoData) => {
+    return cryptoData.map(data => {
+        return {
+            ...data,
+            symbols: {
+                binance: data.symbols.binance || '',
+                kucoin: data.symbols.kucoin || '',
+                bybit: data.symbols.bybit || '',
+                gateio: data.symbols.gateio || '',
+                coinbase: data.symbols.coinbase || '',
+                mexc: data.symbols.mexc || '',
+                okx: data.symbols.okx || ''
+            },
+            rankDiffs: {
+                hour: data.rankDiffs?.hour ?? 0,
+                day: data.rankDiffs?.day ?? 0,
+                week: data.rankDiffs?.week ?? 0,
+                month: data.rankDiffs?.month ?? 0,
+                year: data.rankDiffs?.year ?? 0
+            },
+            performance: {
+                hour: data.performance?.hour ?? 0,
+                min1: data.performance?.min1 ?? 0,
+                min5: data.performance?.min5 ?? 0,
+                min15: data.performance?.min15 ?? 0,
+                day: data.performance?.day ?? 0,
+                week: data.performance?.week ?? 0,
+                month: data.performance?.month ?? 0,
+                year: data.performance?.year ?? 0
+            }
+        };
+    });
+};
 
-app.use(morgan.successHandler);
-app.use(morgan.errorHandler);
 
-app.use(helmet());
+// Fetch and save data function
+const fetchDataAndSave =  async() => {
+    try {
+        const response = await axios.get('https://cryptobubbles.net/backend/data/bubbles1000.usd.json');
+        cryptoData = response.data;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+        cryptoData = validateCryptoData(cryptoData);
 
-app.use(xss());
-app.use(mongoSanitize())
+        await Crypto.deleteMany({});
 
-app.use(compression())
+        await Crypto.insertMany(cryptoData.slice(0,100));
+        console.log('Data saved successfully')
+    } catch(error) {
+        console.log('Error fetching data or saving crypto data', error)
+    }
+};
 
-// enable cors
-app.use(cors());
-app.options('*', cors());
-// v1 api routes
-app.use('/v1', routes);
+cron.schedule('* * * * *', fetchDataAndSave);
 
-// send back a 404 error for any unknown api request
-// app.use((req, res, next) => {
-//     next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
-// });
+let server
+mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
+    logger.info('Connected to MongoDB');
+    server = app.listen(config.port, () => {
+        logger.info('Express server listening on port ' + config.port);
+    })
+}).catch(error => {
+    logger.error('Could not connect to MongoDB. Exiting now...', error);
+    process.exit();
+});
 
-// convert error to ApiError, if needed
-app.use(errorConverter);
 
-// handle error
-app.use(errorHandler);
 
-module.exports = app;
+const exitHandler = () => {
+    if (server) {
+        server.close(() => {
+            logger.info('Server closed');
+            process.exit(1);
+        });
+    } else {
+        process.exit(1);
+    }
+};
+
+const unexpectedErrorHandler = (error) => {
+    logger.error(error);
+    exitHandler();
+}
+
+process.on('uncaughtException', unexpectedErrorHandler);
+process.on('unhandledRejection', unexpectedErrorHandler);
+
+process.on('SIGTERM', () => {
+    logger.info('SIGTERM received');
+    if (server) {
+        server.close();
+    }
+});
