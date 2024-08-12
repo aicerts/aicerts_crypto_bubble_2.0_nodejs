1b95dfa598feeca743b339dce2704d94500df661
Sachint10 <sachin.thokale@aicerts.io>
2024-08-12 17:05:56 +0530

    env changed

--- controllers/crypto.controller.js
+++ controllers/crypto.controller.js
@@ -5,10 +5,8 @@ const catchAsync = require("../utils/catchAsync");
 const cryptoService = require("../services/crypto.service");
 const request = require("request");
 const config = require("../config/config");
-const axios = require("axios");
 const CryptoGraph = require("../models/cryptoGraphModel");
 const CryptoImage = require("../models/cryptoImageModel");
-const { response } = require("express");
 
 const fetchCrypto = catchAsync(async (req, res, next) => {
   try {
@@ -23,11 +21,52 @@ const fetchCrypto = catchAsync(async (req, res, next) => {
   }
 });
 
-const fetchImage = catchAsync(async (req, res, next) => {
+// const fetchImage = catchAsync(async (req, res, next) => {
+//   try {
+//     const { imageName } = req.params;
+//     // Replace with the actual URL where the image is hosted
+//     const imageUrl = `${config.source}/${imageName}`;
+//     request
+//       .get(imageUrl)
+//       .on("error", (err) => {
+//         console.error(`Error fetching image from ${imageUrl}:`, err);
+//         return res
+//           .status(404)
+//           .json({ status: "FAILED", message: "Failed to fetch image" });
+//       })
+//       .on("response", (response) => {
+//         // Modify the response headers to show the desired URL
+//         response.headers[
+//           "Content-Disposition"
+//         ] = `inline; filename="${config.mask}/${imageName}"`;
+//         response.headers["Content-Type"] = "image/png"; // Adjust according to your image type
+//         res.setHeader(
+//           "Content-Disposition",
+//           response.headers["Content-Disposition"]
+//         );
+//       })
+//       .pipe(res);
+//   } catch (error) {
+//     next(error);
+//     throw new ApiError(
+//       httpStatus.INTERNAL_SERVER_ERROR,
+//       "Failed to fetch image data"
+//     );
+//   }
+// });
+const fetchCryptoImage = catchAsync(async (req, res, next) => {
   try {
     const { imageName } = req.params;
-    // Replace with the actual URL where the image is hosted
-    const imageUrl = `${config.source}/${imageName}`;
+    const imageUrl = `${config.source}/data/logos/${imageName}`;
+
+    // Check if the image is already in the database
+    let existingImage = await CryptoImage.findOne({ imageName });
+    if (existingImage) {
+      res.contentType(existingImage.contentType);
+      return res.send(existingImage.imageData);
+    }
+
+    // Fetch the image from the external source
     request
       .get(imageUrl)
       .on("error", (err) => {
@@ -36,88 +75,35 @@ const fetchImage = catchAsync(async (req, res, next) => {
           .status(404)
           .json({ status: "FAILED", message: "Failed to fetch image" });
       })
-      .on("response", (response) => {
-        // Modify the response headers to show the desired URL
-        response.headers[
-          "Content-Disposition"
-        ] = `inline; filename="${config.mask}/${imageName}"`;
-        response.headers["Content-Type"] = "image/png"; // Adjust according to your image type
-        res.setHeader(
-          "Content-Disposition",
-          response.headers["Content-Disposition"]
-        );
-      })
-      .pipe(res);
-  } catch (error) {
-    next(error);
-    throw new ApiError(
-      httpStatus.INTERNAL_SERVER_ERROR,
-      "Failed to fetch image data"
-    );
-  }
-});
-const fetchCryptoImage = catchAsync(async (req, res, next) => {
-  try {
-    const { imageName } = req.params;
-    const imageUrl = `${config.source}/${imageName}`;
-
-    // Check if the image is already in the database
-    let existingImage = await CryptoImage.findOne({ imageName });
-    if (existingImage) {
-      // If found, send the stored image data
-      res.contentType(existingImage.contentType);
-      return res.send(existingImage.imageData);
-    }
-
-    // Fetch the image from the external source
-    request.get(imageUrl)
-    .on("error", (err) => {
-      console.error(`Error fetching image from ${imageUrl}:`, err);
-      return res
-        .status(404)
-        .json({ status: "FAILED", message: "Failed to fetch image" });
-    })
       .on("response", async (response) => {
-        console.log(response)
-      
-       
+        console.log(response);
+
         let imageData = [];
-        response.on('data', (chunk) => {
+        response.on("data", (chunk) => {
           imageData.push(chunk);
         });
 
-        response.on('end', async () => {
+        response.on("end", async () => {
           const buffer = Buffer.concat(imageData);
-
-          // Store the image in MongoDB using create
           const newImage = await CryptoImage.create({
             imageName,
             imageData: buffer,
-            contentType: response.headers['content-type'],
+            contentType: response.headers["content-type"],
           });
-          console.log(newImage)
-
-          // Send the stored image to the frontend
           res.contentType(newImage.contentType);
           res.send(newImage.imageData);
         });
       });
-
   } catch (error) {
     console.error("Error in fetchCryptoImage:", error);
-    next(new ApiError('An error occurred while processing the image', 500));
+    next(new ApiError("An error occurred while processing the image", 500));
   }
 });
-
-
-
 
 const fetchCryptoGraphdata = catchAsync(async (req, res, next) => {
   try {
     const { timeframe, cryptoId, currency } = req.params;
-
-    // Fetch data from the external API
-    const url = `https://cryptobubbles.net/backend/data/charts/${timeframe}/${cryptoId}/${currency}`;
+    const url = `${config.source}/data/charts/${timeframe}/${cryptoId}/${currency}`;
     request
       .get(url)
       .on("error", (err) => {
@@ -160,6 +146,4 @@ module.exports = {
   fetchCrypto,
   fetchCryptoGraphdata,
   fetchCryptoImage,
-
-  fetchImage,
 };
--- routes/crypto.route.js
+++ routes/crypto.route.js
@@ -4,12 +4,12 @@ const cryptoController = require("../controllers/crypto.controller");
 const router = express.Router();
 
 router.get("/fetch-crypto", cryptoController.fetchCrypto);
-router.get("/fetch-image/data/logos/:imageName", cryptoController.fetchImage);
+// router.get("/fetch-image/data/logos/:imageName", cryptoController.fetchImage);
 router.get(
   "/fetch/graph/:timeframe/:cryptoId/:currency",
   cryptoController.fetchCryptoGraphdata
 );
 
-router.get("/fetch/image/data/logos/:imageName", cryptoController.fetchCryptoImage)
+router.get("/fetch/image/:imageName", cryptoController.fetchCryptoImage);
 
 module.exports = router;
